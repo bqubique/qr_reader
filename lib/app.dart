50bea2e0117e418d7a405a800271b2a111100799
bqubique <blinnqipa@gmail.com>
2021-10-06 15:58:03 +0200

    Share information from MainActivity.kt to frontend.

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,52 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:qr_reader/camera_screen.dart';
 
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+class Camera extends StatefulWidget {
+  const Camera({Key? key}) : super(key: key);
+
+  @override
+  State<Camera> createState() => _CameraState();
+}
+
+class _CameraState extends State<Camera> {
+  String? _batteryLevel = '0';
+
+  static const platform = MethodChannel('com.bqubique.qr/camera');
+
+  Future<void> _getCamera() async {
+    String batteryLevel;
+    try {
+      final int result = await platform.invokeMethod('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('_batteryLevel'),
+            Center(
+              child: ElevatedButton(
+                onPressed: _getCamera,
+                child: const Text('Open Camera'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
